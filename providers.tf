f47db7cdff330d9c7a41bcad28360b8c2200891d
Ubuntu <ubuntu@ip-172-31-23-53.us-west-1.compute.internal>
2023-10-22 11:20:34 +0000

    updated jenkins.yml and providers.yml

--- providers.tf
+++ providers.tf
@@ -10,16 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-1"
-  # shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  # shared_config_files      = ["/Users/tf_user/.aws/conf"]
-  # shared_credentials_files = ["/Users/tf_user/.aws/creds"]
-  # profile                  = "customprofile"  
 }
 
-# provider "aws" {
-#   region = "us-west-1"
-#   shared_config_files = ["/Users/tf_user/.aws/conf"]
-#   shared_credentials_files = ["/Users/tf_user/.aws/creds"]
-#   profile = "customprofile"
-# }
 
