55376345beed2398d79f2cbc63de7f3fee1e4a1b
Ubuntu <ubuntu@ip-172-31-23-53.us-west-1.compute.internal>
2023-10-22 08:50:06 +0000

    updated providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,25 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-west-1"
-  shared_credentials_file = "home/ubuntu/.aws/credentials"
+  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  # shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  # shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  # profile                  = "customprofile"  
 }
+
+# provider "aws" {
+#   region = "us-west-1"
+#   shared_config_files = ["/Users/tf_user/.aws/conf"]
+#   shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+#   profile = "customprofile"
+# }
+
