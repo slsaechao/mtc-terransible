fff185f283d7d98164f5d2d6a39dacaaa966bcee
Ubuntu <ubuntu@ip-172-31-21-70.us-west-1.compute.internal>
2023-10-24 09:15:56 +0000

    added output instance_ids to compute.tf & updated Ec2 Wait stage on Jenkinsfile

--- compute.tf
+++ compute.tf
@@ -42,3 +42,7 @@ output "grafana_access" {
 output "instance_ips" {
   value = [for i in aws_instance.mtc_main[*]: i.public_ip]
 }
+
+output "instance_ids" {
+  value = [for i in aws_instance.mtc_main[*]: i.public_id]
+}
